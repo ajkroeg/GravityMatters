a7c5084a7f66e8961cf356eae010c5c475bba1b5
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2020-07-06 19:10:46 -0400

    update to allow gravity/biome to change affect weapon range

--- ModInit.cs
+++ ModInit.cs
@@ -29,10 +29,19 @@ namespace GravityMatters
     public class GravityMattersSettings
     {
         public bool UsePlanetGravityTags = false;
+        public bool BuffWeaponRange = false;
+        public bool BuffAllRangeBrackets = false;
+        public bool LetGravityAffectEnergyWeps = false;
         public float LunarJumpOverride = 1.2f;
         public float MartianJumpOverride = 1.2f;
         public float lowgravJumpOverride = 1.2f;
         public float highgravJumpOverride = 0.8f;
+
+        public float LunarWeaponOverride = 1.1f;
+        public float MartianWeaponOverride = 1.1f;
+        public float lowgravWeaponOverride = 1.1f;
+        public float highgravWeaponOverride = 0.9f;
+
         public bool JumpHeatOverrideFlag = false;
         public float JumpHeatOverrideMultiplier = 1f;
     }
